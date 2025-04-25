86d4846f1f18eb4c90b97d2e4535979e4a4342eb
DataNext <next.data134@gmail.com>
2025-04-25 22:07:00 +0200

    Created SonarListenerNotConnected.cs and updated SonarSocket.cs

---
+++ SteelSeriesAPI/Sonar/Exceptions/SonarListenerNotConnected.cs
@@ -0,0 +1,8 @@
+﻿namespace SteelSeriesAPI.Sonar.Exceptions;
+
+public class SonarListenerNotConnected : Exception
+{
+    public SonarListenerNotConnected() : base("Listener need to be connected before listening") { }
+    public SonarListenerNotConnected(string message) : base(message) { }
+    public SonarListenerNotConnected(string message, Exception innerException) : base(message, innerException) { }
+}
